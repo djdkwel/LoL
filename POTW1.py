82305fdd1d7c1412bdd055fb48a60fecec54c42c
djdkwel <44046752+djdkwel@users.noreply.github.com>
2019-02-03 19:05:41 -0500

    Update POTW1.py

--- POTW1.py
+++ POTW1.py
@@ -1,19 +1,11 @@
-def LoL(n,e):
-    totalEnemies = 1
-    totalEnemies+=e
+def findMinGameCycle(e, n, mini):
     count = 1
-    y=[]
-    y.append(totalEnemies)
-    x=[]
-    x.append(e)
+    totalMini = mini
     e+=1
-    x.append(e)
-    while(count<n):
-        totalEnemies*=2
-        y.append(totalEnemies)
-        e*=2
-        x.append(e)
+    e*=2**(n-1)
+    while(totalMini<e):
+        mini+=1
         count+=1
-    print(x)
-    print(y)
-    print(count)
+        n+=count
+        totalMini+=mini       
+    return n
